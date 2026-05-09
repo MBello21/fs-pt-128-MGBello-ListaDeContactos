d6a64091ab3ee31b8651d81500dad91233d7bc9c
Miguel Ángel García Bello <magarbello@gmail.com>
2026-05-09 13:45:20 +0200

    Update Footer component text alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="footer text-light mt-auto py-3 text-center position-absolute bottom-0 start-0 end-0 bg-dark text-center">
-      <p className="m-0">
+      <p className="m-0 text-center">
         <a href="https://4geeksacademy.slack.com/team/U0985A80P24">
           <i className="fa-solid fa-copyright mx-1"></i>2026 Migue Garcia Bello</a>
       </p>
